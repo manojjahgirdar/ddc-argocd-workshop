e1ff1ba32c1e99465c64460e758192492d737a40
manojjahgirdar <manoj.jahgirdar@in.ibm.com>
2021-08-12 17:14:40 +0530

    Deployment of image manojjahgirdar/ddc-argocd-demo:v6

--- code/application.py
+++ code/application.py
@@ -3,13 +3,23 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 import logging as logger
 import os
+import platform
 
 application = Flask(__name__)
 application.config['SECRET_KEY'] = 'supersecretkey'
 
 @application.route('/')
 def index():
-    return render_template('index.html')
+    details = {
+        "machine": platform.machine(),
+        "platform": platform.platform(),
+        "processor": platform.processor(),
+        "python_version": platform.python_version(),
+        "release": platform.release(),
+        "system": platform.system(),
+        "version": platform.version()
+    }
+    return render_template('index.html', details=details)
 
 port = os.getenv('VCAP_APP_PORT', '8080')
 if __name__ == "__main__":
